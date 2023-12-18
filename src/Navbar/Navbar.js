795027cb70345790fe7652defc4dbd9029d46ca3
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-12-19 00:24:04 +0530

    Added icon to navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import style from './navbar.module.css'
+import picture from './picture.png'
 
 export default function Navbar(){
     return <>
     <div className="App">
         <header className={style.header}>
+            <img src={picture} height='100%'/>
             <h2>PhotoFolio</h2>
         </header>
         <Outlet/>
